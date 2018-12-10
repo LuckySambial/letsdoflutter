0b78b1690ee79dab5070f4423f131d6e277e90d0
Dinesh <dineshrajput@ucreate.co.in.com>
2018-12-10 18:00:41 +0530

    initial commit

---
+++ android/app/src/main/java/com/example/letsdoit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.letsdoit;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ build/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: /Users/ucreate19/FlutterStuff/Setup/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
